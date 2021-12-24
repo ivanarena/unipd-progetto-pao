1f7e42bcb47b21c45ac0f6bee09c6e97b516af10
Ivan Arena <ivan.x28x@gmail.com>
2021-12-24 15:11:52 +0100

    Add toolbar and tabs widget

--- view.cpp
+++ view.cpp
@@ -6,7 +6,12 @@
 #include <QGridLayout>
 #include <QVXYModelMapper>
 #include <QHeaderView>
+#include <QTabWidget>
+#include <QToolButton>
+#include <QToolBar>
+#include <QMenuBar>
 
+// TODO: FARE UNA CLASSE PER OGNI CHART ED IMPLEMENTARE QUESTO METODO COME UNICO createChart POLIMORFO
 QChart * View::createLineChart(DataTableModel *model)
 {
     QChart *lineChart = new QChart();
@@ -59,12 +64,47 @@ View::View(QWidget *parent)
 
 
     // MAIN LAYOUT + ADJUST LAYOUT AUTOMATICALLY
+    QTabWidget *tabView= new QTabWidget;
+
+    QWidget *mainWindow = new QWidget;
+
+    QGridLayout *sceneLayout = new QGridLayout(mainWindow);
+    sceneLayout->addWidget(tableView, 1, 0);
+    sceneLayout->addWidget(chartView, 1, 1);
+    sceneLayout->setColumnStretch(0, 2);
+    sceneLayout->setColumnStretch(1, 3);
+
+    mainWindow->setLayout(sceneLayout);
+
+    tabView->addTab(mainWindow, "Model");
+    //tabView->show(); // PROBABILMENTE NON SERVE PERCHÉ HO FIXATO
+
+
     QGridLayout *mainLayout = new QGridLayout;
-    mainLayout->addWidget(tableView, 1, 0);
-    mainLayout->addWidget(chartView, 1, 1);
-    mainLayout->setColumnStretch(0, 2);
-    mainLayout->setColumnStretch(1, 3);
+
+    QToolBar *toolBar = new QToolBar;
+    toolBar->setOrientation(Qt::Vertical);
+
+    // TODO: LE AZIONI DOVREBBERO ESSERE CREATE ALTROVE ED ESSERE USATE UGUALI ANCHE NEL MENU
+    toolBar->addAction("New");
+    toolBar->addAction("Open");
+    toolBar->addAction("Save");
+    toolBar->addAction("+Tab");
+    toolBar->addAction("-Tab");
+    toolBar->addAction("+row");
+    toolBar->addAction("-row");
+    toolBar->addAction("+col");
+    toolBar->addAction("-col");
+
+    mainLayout->addWidget(toolBar, 1, 0);
+    mainLayout->addWidget(tabView, 1, 1);
     setLayout(mainLayout);
+
+    // ! TUTTO QUELLO QUA SOPRA FUNZIONA
+
+
+
+
 }
 
 View::~View()
