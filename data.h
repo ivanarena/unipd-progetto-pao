499060c6dccb992a4a8a553f24e4d861d4c76336
Lorenzopensource <72146782+Lorenzopensource@users.noreply.github.com>
2022-01-14 11:29:56 +0100

    Add files via upload

--- data.h
+++ data.h
@@ -11,17 +11,17 @@ using std::vector;
 class Data
 {
 private:
-    int rowsize; //quante colonne,
+    unsigned int rowsize; //quante colonne,
     vector<string> labels;
     vector<vector<int>> rows;
 public:
-    explicit Data(int n = 0, const vector<string>& = vector<string>() , const vector<vector<int>>& = vector<vector<int>>());
+    explicit Data(unsigned int n = 0, const vector<string>& = vector<string>() , const vector<vector<int>>& = vector<vector<int>>());
     void insertCol(const vector<int>&);
     void insertRow(string s, const vector<int>&);
     void removeCol();
     void removeRow();
-    vector<int> readRow(int) const;
-    string readLabel(int) const;
+    vector<int> readRow(unsigned int) const;
+    string readLabel(unsigned int) const;
 };
 
 #endif // DATA_H
